b5c70b6bc2ae5d94f2da7e1acc843f3420d35a2c
Siddami <dharmmy621@gmail.com>
2025-06-24 08:26:21 +0100

    fix: motion div error by adding as  Motion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,17 @@
-import { motion } from "framer-motion";
+import { motion as Motion } from "framer-motion";
 import profileImg from "../assets/sid.png";
-import { Navbar } from "./Navbar";
+import Navbar  from "./Navbar";
 
-export default function Header() {
+function Header() {
   return (
-    <Header className="relative min-h-screen bg-white text-black dark:bg-gray-300 dark:text-white">
+    <header className="relative min-h-screen bg-white text-black dark:bg-gray-300 dark:text-white">
       {/* Navigation */}
       <Navbar />
 
       {/* Hero Content */}
       <section className="flex flex-col items-center justify-center h-screen px-4 sm:px-6 md:px-10">
         {/* Image */}
-        <motion.img
+        <Motion.img
           src={profileImg}
           alt="Profile"
           className="w-[80vw] sm:w-[60vw] md:w-[40vw] max-w-xs md:max-w-md object-cover z-10"
@@ -22,24 +22,26 @@ export default function Header() {
 
         {/* Name Text */}
         <div className="w-full max-w-4xl mt-6 text-center">
-          <motion.h1
+          <Motion.h1
             initial={{ opacity: 0, y: 10 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ delay: 0.6, duration: 0.8 }}
             className="text-[10vw] sm:text-[5vw] md:text-[7vw] leading-none font-bold text-gray-200 dark:text-gray-800"
           >
             siddiqat
-          </motion.h1>
-          <motion.h2
+          </Motion.h1>
+          <Motion.h2
             initial={{ opacity: 0, y: 10 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ delay: 0.8, duration: 0.8 }}
             className="text-[10vw] sm:text-[5vw] md:text-[7vw] leading-none font-bold text-gray-200 dark:text-gray-800"
           >
             Abdulkareem
-          </motion.h2>
+          </Motion.h2>
         </div>
       </section>
-    </Header>
+    </header>
   );
 }
+
+export default Header;
